fa297f330f659e9e2752edf886db932ec209743b
ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-10-24 19:24:25 +0200

    changet crud to jpa repository

--- src/main/java/com/japanese/lessons/repositories/Lesson/ILessonRepository.java
+++ src/main/java/com/japanese/lessons/repositories/Lesson/ILessonRepository.java
@@ -1,7 +1,8 @@
 package com.japanese.lessons.repositories.Lesson;
 
 import com.japanese.lessons.models.lesson.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ILessonRepository extends CrudRepository<Lesson, Long> {
+public interface ILessonRepository extends JpaRepository<Lesson, Long> {
 }
