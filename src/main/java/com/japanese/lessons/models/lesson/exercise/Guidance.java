55efbf27b664f6b45c1aaaf3778dad2d1c080626
ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-12-01 13:27:26 +0100

    add mex size to guidance

--- src/main/java/com/japanese/lessons/models/lesson/exercise/Guidance.java
+++ src/main/java/com/japanese/lessons/models/lesson/exercise/Guidance.java
@@ -1,6 +1,7 @@
 package com.japanese.lessons.models.lesson.exercise;
 import com.japanese.lessons.models.EGuidanceType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ public class Guidance {
     private String topic;
 
     @Column
+    @Size(max = 500)
     private String description;
 
     @Enumerated(EnumType.STRING)
